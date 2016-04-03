3e6b933a0a2a2908d6420218acd371661dbcc4b2
nathan-russell <russell.nr2012@gmail.com>
2016-04-03 15:53:50 -0400

    First pass at show method

--- R/classes.R
+++ R/classes.R
@@ -8,3 +8,24 @@
 #' @exportClass Rcpp_Hashmap
 #' @include hashmap.R
 NULL
+
+setMethod("show", "Rcpp_Hashmap",
+    function(object) {
+        if (object$empty()) {
+            cat("\n", "[empty Hashmap]", "\n")
+            return(invisible(object))
+        }
+
+        .data <- head(object$data(), 5)
+        .keys <- sprintf("[%s]", as.character(names(.data)))
+        .values <- sprintf("[%s]", as.character(unname(.data)))
+        .width <- max(c(nchar(.keys), nchar(.values)))
+        .txt <- sprintf(
+            "%s => %s",
+            formatC(.keys, width = .width),
+            formatC(.values, width = .width)
+        )
+        cat(.txt, sep = "\n")
+        invisible(object)
+    }
+)
