818dc7d017deeabb02813a545181c44e96844348
nathan-russell <russell.nr2012@gmail.com>
2016-10-31 13:28:18 -0400

    Standardize plugin

--- R/plugin.R
+++ R/plugin.R
@@ -12,9 +12,27 @@
 #'  \code{// [[Rcpp::plugins(hashmap)]]} and \code{#include <hashmap.h>}
 #'  to the top of their Rcpp source files.
 plugin <- Rcpp:::Rcpp.plugin.maker(
-    libs = sprintf(
-        "%s/hashmap/libs/hashmap.so",
-        installed.packages()["hashmap","LibPath"][1]
-    ),
+    libs = (function() {
+        libs_path <- sprintf(
+            "%s/hashmap/libs",
+            installed.packages()["hashmap","LibPath"][1]
+        )
+
+        pattern <- sprintf("*hashmap%s", .Platform$dynlib.ext)
+        lib_files <- list.files(
+            libs_path,
+            pattern = pattern,
+            full.names = TRUE,
+            recursive = TRUE
+        )
+
+        if (length(lib_files) < 1) {
+            res <- "failed-to-locate-hashmap-shared-library"
+        } else {
+            res <- lib_files[1]
+        }
+
+        res
+    })(),
     package = "hashmap"
 )
